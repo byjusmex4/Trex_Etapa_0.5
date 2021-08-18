5d6ba256d36f8b14c5cdc6554519c9e99fd91646
byjusmex4 <88998245+byjusmex4@users.noreply.github.com>
2021-08-18 16:03:45 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var trex, trex_running, edges;
+var groundImage;
+
+function preload(){
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  groundImage = loadImage("ground2.png")
+}
+
+function setup(){
+  createCanvas(600,200);
+  
+  //crear sprite de Trex
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  edges = createEdgeSprites();
+  
+  //agregar tamaño y posición al Trex
+  trex.scale = 0.5;
+  trex.x = 50
+}
+
+
+function draw(){
+  //Dar color al fondo.
+  background("white");
+  
+  //cargar la posición Y del Trex
+  console.log(trex.y)
+  
+  //hacer que el Trex salte al presionar la barra espaciadora
+  if(keyDown("space")){
+    trex.velocityY = -10;
+  }
+  
+  trex.velocityY = trex.velocityY + 0.5;
+  
+  //evitar que el Trex caiga
+  trex.collide(edges[3])
+  drawSprites();
+}
